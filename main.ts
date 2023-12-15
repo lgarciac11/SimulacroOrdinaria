5e874fa7a7b9a6667c5e79e8b41eede2be420e82
Laura García <lgarciac11@alumnos.nebrija.es>
2023-12-15 16:36:24 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { ApolloServer} from "@apollo/server";
+import { GraphQLHTTP } from "gql";
+import { makeExecutableSchema } from "graphql_tools";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { Query } from "./resolvers/query.ts";
+import { Character } from "./resolvers/character.ts";
+import { Episode } from "./resolvers/episode.ts";
+import { typeDefs } from "./schema.ts";
+
+const resolvers = {
+  Query,
+  Character,
+  Episode,
+  Location,
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Character,
+    Episode
+  }
+});
+
+const { url } = await startStandaloneServer(server);
+console.log(`🚀 Server ready at ${url}`);
+
